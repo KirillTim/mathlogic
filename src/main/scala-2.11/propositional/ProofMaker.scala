3365d6ac0ed05a0e14a3d1aaffeb3b84dfbffde2
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2015-11-18 21:53:55 +0300

    hw3 done, not tested

--- src/main/scala-2.11/propositional/ProofMaker.scala
+++ src/main/scala-2.11/propositional/ProofMaker.scala
@@ -1,12 +1,13 @@
 package propositional
 
-import propositional.ExprTypes.Var
+import propositional.ExprTypes.{!!, Var}
 import propositional.Types.{Statement, NotTrue, ErrorReason, Proof}
 import scala.collection.{mutable => m}
-import propositional.Proofs
+import propositional.Proofs.buildProof
 
 class ProofMaker {
   type Hypothesis = Map[String, Boolean]
+
   def apply(fileName: String): Either[ErrorReason, Proof] = {
     null
   }
@@ -14,54 +15,70 @@ class ProofMaker {
   def apply(expr: Expr): Either[ErrorReason, List[Expr]] = {
     whenFalse(expr) match {
       case Some(q: Map[String, Boolean]) => Left(new NotTrue(varsToString(q)))
-      case _ => Right(makeProof(expr))
+      case _ => Right(makeNotAnnotatedProof(expr).toList)
     }
   }
 
-  def makeProof(expr: Expr) = {
-    null
+  def makeNotAnnotatedProof(expr: Expr):Seq[Expr] = {
+    var curVars = expr.getVars
+    var proofs = createVarsToBoolList(curVars).map( (hypot:Hypothesis) => hypot -> buildProof(expr, hypot)).toMap
+    while (curVars.nonEmpty) {
+      val tp = removeAssumption(curVars, proofs, expr)
+      proofs = tp._2
+      curVars = tp._1
+    }
+    proofs.head._2
   }
 
-  def removeAssumption(vars:m.HashSet[String], proofs: Map[Hypothesis, Seq[Expr]]) : Map[Hypothesis, Seq[Expr]] = {
-    if (vars.isEmpty)
-      proofs
-    else {
-      val smallerVars = vars.take(vars.size - 1)
-      val smallerVarsList = createVarsToBoolList(smallerVars)
-      var newProofs = m.HashMap[Hypothesis, Seq[Expr]]()
-      for (i <- smallerVarsList) {
-        var proofsToMerge = m.MutableList[Seq[Expr]]()
-        var varToRemove = ""
-        for (j <- proofs) {
-          val hypot = j._1
-          if (checkVars(i, hypot)) {
-            proofsToMerge += j._2
-            varToRemove = hypot.keySet.filterNot(i.keySet).head
+  def removeAssumption(vars: m.HashSet[String], proofs: Map[Hypothesis, Seq[Expr]],
+                       beta: Expr): (m.HashSet[String], Map[Hypothesis, Seq[Expr]]) = {
+    vars.isEmpty match {
+      case true => (vars, proofs)
+      case false => {
+        val smallerVars = vars.take(vars.size - 1)
+        val smallerVarsList = createVarsToBoolList(smallerVars)
+        var newProofs = Map[Hypothesis, Seq[Expr]]()
+        for (i <- smallerVarsList) {
+          var proofsToMerge = m.MutableList[Seq[Expr]]()
+          var varToRemove = ""
+          for (j <- proofs) {
+            val hypot = j._1
+            if (checkVars(i, hypot)) {
+              proofsToMerge += j._2
+              varToRemove = hypot.keySet.filterNot(i.keySet).head
+            }
           }
+          //proofsToMerge.size == 2
+          if (proofsToMerge.size != 2)
+            throw new Error("proofsToMerge.size != 2")
+          if (varToRemove.isEmpty)
+            throw new Error("varToRemove.isEmpty")
+          var newProof = List[Expr]()
+          val deduceContext = i.keys.map((e: String) => new Var(e)).toSeq
+          val deduced0 = new Deductor().apply(deduceContext, Some(proofsToMerge.head.last), proofsToMerge.head) match {
+            case Left(proof) => {throw new Error("wrong proof"); null}
+            case Right(proof) => (smallerVars, proof.map((a: Statement) => a.expr))
+          }
+          val deduced1 = new Deductor().apply(deduceContext, Some(proofsToMerge(1).last), proofsToMerge(1)) match {
+            case Left(proof) => {throw new Error("wrong proof"); null}
+            case Right(proof) => proof.map((a: Statement) => a.expr)
+          }
+          newProof ++= (deduced0 ++ deduced1 ++ Proofs.tertiumNonDatur(new Var(varToRemove)))
+          val pVar = new Var(varToRemove)
+          newProof ++= List[Expr](
+            (pVar ->: beta) ->: (!!(pVar) ->: beta) ->: (pVar V !!(pVar)) ->: beta,
+            (!!(pVar) ->: beta) ->: ((pVar V !!(pVar)) ->: beta),
+            (pVar V !!(pVar)) ->: beta,
+            beta)
+          newProofs += (i -> newProof.toSeq)//.put(i, newProof.toSeq)
         }
-        //proofsToMerge.size == 2
-        if (proofsToMerge.size != 2)
-          throw new Error("proofsToMerge.size != 2")
-        if (varToRemove.isEmpty)
-          throw new Error("varToRemove.isEmpty")
-        var newProof = List[Expr]()
-        val deduceContext = i.keys.map((e:String) => new Var(e)).toSeq
-        val deduced0 = new Deductor().apply(deduceContext, Some(proofsToMerge.head.last), proofsToMerge.head) match {
-          case Left(proof) => throw new Error("wrong proof") null
-          case Right(proof) => proof.map((a:Statement) => a.expr)
-        }
-        val deduced1 = new Deductor().apply(deduceContext, Some(proofsToMerge(1).last), proofsToMerge(1)) match {
-          case Left(proof) => throw new Error("wrong proof") null
-          case Right(proof) => proof.map((a: Statement) => a.expr)
-        }
-        newProof ++= (deduced0 ++ deduced1 ++ Proofs.tertiumNonDatur(new Var(varToRemove)))
-
+        (smallerVars, newProofs)
       }
     }
   }
   //проверка, что все ключи из первого map есть во втором и все ключи из первого имеют одинаковые занчения в первом и втором
   //первый меньше чем второй
-  def checkVars(first:Hypothesis,second:Hypothesis) : Boolean = {
+  def checkVars(first: Hypothesis, second: Hypothesis): Boolean = {
     for (i <- first) {
       second.get(i._1) match {
         case Some(a) => if (a != i._2) return false
@@ -71,7 +88,7 @@ class ProofMaker {
     true
   }
 
-  def createVarsToBoolList(vars:m.HashSet[String]): List[Hypothesis] = {
+  def createVarsToBoolList(vars: m.HashSet[String]): List[Hypothesis] = {
     gen(vars.size).map(x => vars.zip(x).toMap)
   }
 
@@ -89,7 +106,7 @@ class ProofMaker {
         case Some(_) => true
         case _ => false
       }) match {
-      case Some(a) => a//a.get
+      case Some(a) => a //a.get
       case _ => None
     }
   }
